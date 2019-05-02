611f813b4caaef109855af859d078a40d87af02a
Bikash Das <bikashdaseee@gmail.com>
2019-05-02 09:18:28 +0600

    New test class created.

---
+++ src/test/kotlin/com/dasbikash/news_server_parser_rest_end_point/NewsServerParserRestEndPointApplicationTest.java
@@ -0,0 +1,17 @@
+package com.dasbikash.news_server_parser_rest_end_point;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class NewsServerParserRestEndPointApplicationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
