76516be0d26c23e112a4d28dca061986eb35c242
Bikash Das <bikashdaseee@gmail.com>
2019-05-19 09:58:25 +0600

    'MAX_ENTRY_DELETE_LIMIT' increased to 500 for 'LogEntryDeleteRequest'

--- src/main/kotlin/com/dasbikash/news_server_parser_rest_end_point/model/LogEntryDeleteRequest.kt
+++ src/main/kotlin/com/dasbikash/news_server_parser_rest_end_point/model/LogEntryDeleteRequest.kt
@@ -8,7 +8,7 @@ class LogEntryDeleteRequest(
         var entryDeleteCount: Int? = null
 ) : NsParserRestDbEntity {
     companion object {
-        const val MAX_ENTRY_DELETE_LIMIT = 50
+        const val MAX_ENTRY_DELETE_LIMIT = 500
         const val DEFAULT_ENTRY_DELETE_COUNT = 10
     }
 }
